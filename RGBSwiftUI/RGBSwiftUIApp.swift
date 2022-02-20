bc6525ca2c0c59a0c9e47ec284274ee3aa09f98d
Delina Dvorzhetskaya <delina.dvorzhetskaya@gmail.com>
2022-02-20 22:53:36 +0300

    Initial Commit

---
+++ RGBSwiftUI/RGBSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RGBSwiftUIApp.swift
+//  RGBSwiftUI
+//
+//  Created by Дэлина Дворжецкая on 20.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct RGBSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
